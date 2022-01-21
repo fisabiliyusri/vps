8d3103575bcb34d1e2b12d60e56ff7d6cb23b249
Gandring15 <86754157+Gandring15@users.noreply.github.com>
2022-01-21 15:52:56 +0700

    Update bebek.sh

--- bebek.sh
+++ bebek.sh
@@ -9,7 +9,7 @@ color2='\e[34;1m'
 color3='\e[0m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Gandring15/izin/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -25,9 +25,9 @@ echo -e "${color1}2${color3}.$bd Backup Manual Data VPS (${color2}backup${color3
 echo -e "${color1}3${color3}.$bd Restore Data VPS (${color2}restore${color3})"
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e""
-read -p " Please Enter The Number  [1-3 or x] :  "  gok
+read -p " Please Enter The Number  [1-3 or x] :  "  wisnu
 echo -e ""
-case $gok in
+case $wisnu in
 1)
 autobackup
 ;;
@@ -41,8 +41,8 @@ x)
 menu
 ;;
 *)
-echo "Masukkan Nomor Yang Ada Sayang!"
+echo "please enter the number"
 sleep 1
-bebek
+wisnu
 ;;
 esac
