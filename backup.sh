0adaa89c6dff9cef59e6085933c74fd8cfc55c69
Gandring15 <86754157+Gandring15@users.noreply.github.com>
2022-01-21 15:21:36 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/pegasusq_governor
+# My Telegram : https://t.me/zerossl
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -20,7 +20,7 @@ echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/pegasusq_governor"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/zerossl"
 exit 0
 fi
 clear
