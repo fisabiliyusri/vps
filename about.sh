0ced6b5cb16a7426135e3054d1937d04b0c78098
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-23 10:43:28 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,12 +2,12 @@
 
 clear
 echo -e "================================================="
-echo -e "#         Premium Auto Script By geovpn         #"
+echo -e "#         Premium Auto Script By gandring         #"
 echo -e "================================================="
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By geovpn                            #"
+echo -e "# Build Up By gandring                            #"
 echo -e "================================================="
 echo -e "# Thanks To                                     #"
 echo -e "================================================="
