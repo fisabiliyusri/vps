e5251dbb965c4a1da306f130baeb0a8b8e367cbd
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 04:22:46 +0700

    Update dell2tp.sh

--- dell2tp.sh
+++ dell2tp.sh
@@ -26,8 +26,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/gandring/data-user-l2tp")
 	echo ""
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-	echo " ♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-	echo "     No  Expired   User"
+	echo " ♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+	echo "   nggak ada pengguna kadal"
 	grep -E "^### " "/var/lib/gandring/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
