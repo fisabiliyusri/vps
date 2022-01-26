0f54ca10e68d30f44c250ba621a991be07962311
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-26 05:47:10 +0000

    Update addl2tp.sh

--- addl2tp.sh
+++ addl2tp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/sampiiiiu
+# My Telegram : https://t.me
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -77,5 +77,5 @@ Password   : $VPN_PASSWORD
 Created    : $hariini
 Expired    : $exp
 ============================
-Script By geovpn
+Script By gandring
 EOF
