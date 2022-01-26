d49dfd12afb312ce73d81d88262e673b6c7587f7
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 03:56:52 +0700

    Update delssh.sh

--- delssh.sh
+++ delssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/geovpn
+# My Telegram : https://t.me/gandring
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,17 +12,8 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
-exit 0
-fi
 clear
 read -p "Username SSH to Delete : " Pengguna
 
@@ -31,4 +22,5 @@ if getent passwd $Pengguna > /dev/null 2>&1; then
         echo -e "Username $Pengguna Telah Di Hapus"
 else
         echo -e "Failure: Username $Pengguna Tidak Ada"
+menu
 fi
