2818cbfd1a27fbc3daa65bde5b9809d605625924
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-26 09:14:10 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,74 +4,17 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo -e  "${RED}Checking VPS${NC}"
-sleep 2
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${GREEN}Permission Accepted...${NC}"
-sleep 2 
-else
 clear
-echo -e ""
-echo -e "======================================="
-echo -e ""
-echo -e "${RED}PERMINTAAN DITOLAK...!!! ${NC}"
-echo -e "IP VPS ANDA BELUM TERDAFTAR"
-echo -e "Contact WA https//wa.me/+6282339191527"
-echo -e "For Registration IP VPS"
-echo -e ""
-echo -e "======================================="
-echo -e ""
-exit 0
-fi
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-clear
-echo "Untuk Melakukan Tindakan Ini, Anda Harus Laporan Terlebih Dahulu Kepada Pihak Admin."
-echo "Agar Diberikan Akses Pembaruan Pada Script VPS Anda!"
-read -p "Sudah Laporan? [Y/N]:" arg
-if [[ $arg == 'Y' ]]; then
-  echo "Tindakan Diteruskan!"
-  figlet -f slant Memperbarui... | lolcat
-elif [[ $arg == 'y' ]]; then
-  echo "Tindakan Diteruskan!"
-  figlet -f slant Memperbarui... | lolcat
-elif [[ $arg == 'N' ]]; then
-  echo "Tindakan Dihentikan!"
-  sleep 1
-  clear
-  neofetch
-  exit 0
-elif [[ $arg == 'n' ]]; then
-  echo "Tindakan Dihentikan!"
-  sleep 1
-  clear
-  neofetch
-  exit 0
-else
-  echo "Argumen Tidak Diketahui!"
-  sleep 1
-  clear
-  neofetch
-  exit 0
-fi
-sleep 1
 
 if [ ! -e /home/vps/public_html/TCP.ovpn ]; then
-cp /etc/openvpn/client-tcp-1194.ovpn /home/vps/public_html/tcp.ovpn
-cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/udp.ovpn
+cp /etc/openvpn/client-tcp-700.ovpn /home/vps/public_html/tcp.ovpn
+cp /etc/openvpn/client-udp-800.ovpn /home/vps/public_html/udp.ovpn
 cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/ssl.ovpn
 
 mkdir /root/OpenVPN
 cp -r /etc/openvpn/client-tcp-ssl.ovpn OpenVPN/ssl.ovpn
-cp -r /etc/openvpn/client-udp-2200.ovpn OpenVPN/udp.ovpn
-cp -r /etc/openvpn/client-tcp-1194.ovpn OpenVPN/tcp.ovpn
+cp -r /etc/openvpn/client-udp-800.ovpn OpenVPN/udp.ovpn
+cp -r /etc/openvpn/client-tcp-700.ovpn OpenVPN/tcp.ovpn
 cd /root
 zip -r openvpn.zip OpenVPN > /dev/null 2>&1
 cp -r /root/openvpn.zip /home/vps/public_html/geo.zip
@@ -84,12 +27,12 @@ apt-get install boxes
 # color text
 cd
 # banner /etc/issue.net
-wget -O /etc/issue.net "https://vpnkuy.site/file/banner.conf"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/Gandring15/vps/main/file/banner.conf"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 cd /usr/bin
-wget -O /usr/bin/system https://github.com/geovpn/scriptvps/raw/main/menu/system.sh && chmod +x /usr/bin/system && cd /usr/bin && apt install -y dos2unix && dos2unix system
+wget -O /usr/bin/system https://raw.githubusercontent.com/Gandring15/vps/main/menu/system.sh && chmod +x /usr/bin/system && cd /usr/bin && apt install -y dos2unix && dos2unix system
 
 echo "0 0 * * * root clear-log && xp" >> /etc/crontab
 echo "*/10 * * * * root xp-ws" >> /etc/crontab
