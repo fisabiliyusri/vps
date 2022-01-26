11bd19abcde66f55156d9bc7a0329a14b6a8cf24
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 04:03:24 +0700

    Update delpptp.sh

--- delpptp.sh
+++ delpptp.sh
@@ -16,7 +16,7 @@ MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
 clear
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-pptp")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/gandring/data-user-pptp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -28,7 +28,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-pptp")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/var/lib/geovpnstore/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/var/lib/gandring/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select One Client[1]: " CLIENT_NUMBER
@@ -37,11 +37,11 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-pptp")
 		fi
 	done
 # match the selected number to a client name
-VPN_USER=$(grep -E "^### " "/var/lib/geovpnstore/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/var/lib/geovpnstore/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+VPN_USER=$(grep -E "^### " "/var/lib/gandring/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/var/lib/gandring/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 # Delete VPN user
 sed -i '/^"'"$VPN_USER"'" pptpd/d' /etc/ppp/chap-secrets
-sed -i "/^### $VPN_USER $exp/d" /var/lib/geovpnstore/data-user-pptp
+sed -i "/^### $VPN_USER $exp/d" /var/lib/gandring/data-user-pptp
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
 clear
