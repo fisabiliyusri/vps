ef206ecc2d00e83437d8a0cd15643c516c6942d1
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 04:10:26 +0700

    Update delsstp.sh

--- delsstp.sh
+++ delsstp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/geovpn
+# My Telegram : https://t.me/pegasusq_governor
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,19 +12,9 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
-exit 0
-fi
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-sstp")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/gandring/data-user-sstp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -36,7 +26,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-sstp")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/var/lib/geovpnstore/data-user-sstp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/var/lib/gandring/data-user-sstp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Pilih salah satu[1]: " CLIENT_NUMBER
@@ -44,17 +34,19 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/geovpnstore/data-user-sstp")
 			read -rp "Pilih salah satu [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-Client=$(grep -E "^###" /var/lib/geovpnstore/data-user-sstp | cut -d ' ' -f 2-3 | sed -n "$CLIENT_NUMBER"p)
-user=$(grep -E "^###" /var/lib/geovpnstore/data-user-sstp | cut -d ' ' -f 2 | sed -n "$CLIENT_NUMBER"p)
-exp=$(grep -E "^###" /var/lib/geovpnstore/data-user-sstp | cut -d ' ' -f 3 | sed -n "$CLIENT_NUMBER"p)
-sed -i "/^### $user $exp/d" /var/lib/geovpnstore/data-user-sstp
+Client=$(grep -E "^###" /var/lib/gandring/data-user-sstp | cut -d ' ' -f 2-3 | sed -n "$CLIENT_NUMBER"p)
+user=$(grep -E "^###" /var/lib/gandring/data-user-sstp | cut -d ' ' -f 2 | sed -n "$CLIENT_NUMBER"p)
+exp=$(grep -E "^###" /var/lib/gandring/data-user-sstp | cut -d ' ' -f 3 | sed -n "$CLIENT_NUMBER"p)
+sed -i "/^### $user $exp/d" /var/lib/gandring/data-user-sstp
 sed -i '/^'"$user"'/d' /home/sstp/sstp_account
 clear
 echo ""
-echo "=========================="
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "   SSTP Account Deleted   "
-echo "=========================="
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
-echo "=========================="
-echo "Script By geovpn"
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo "Script By gandring"
+menu
+fi
