3c7a2b0f68cfa8b500e8367f6f3aead435433287
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 03:55:37 +0700

    Update delss.sh

--- delss.sh
+++ delss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/geovpn
+# My Telegram : https://t.me/pegasusq_governor
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,17 +12,8 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/shadowsocks-libev/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -62,10 +53,12 @@ rm -f "/etc/shadowsocks-libev/$user-tls.json"
 rm -f "/etc/shadowsocks-libev/$user-http.json"
 clear
 echo ""
-echo "==========================="
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "  SS OBFS Account Deleted  "
-echo "==========================="
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
-echo "==========================="
-echo "Script By geovpn"
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo "Script By gandring"
+menu
+fi
