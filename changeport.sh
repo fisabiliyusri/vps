f930741ae13473d365dce2c006266fbbfcb5e9a0
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 03:43:21 +0700

    Update changeport.sh

--- changeport.sh
+++ changeport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/geovpn
+# My Telegram : https://t.me/pegasusq_governor
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,21 +12,11 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
-exit 0
-fi
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
 echo -e ""
-echo -e "======================================"
-echo -e ""
+echo -e "     MENU GANTI PORT LAYANAN     "
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "[1]. Change Port OpenVPN"
 echo -e "[2]. Change Port Wireguard"
 echo -e "[3]. Change Port Vmess"
@@ -36,7 +26,7 @@ echo -e "[6]. Change Port Squid"
 echo -e "[7]. Change Port SSTP"
 echo -e "[8]. Exit"
 echo -e ""
-echo -e "======================================"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e ""
 read -p "Select From Options [ 1-8 ] : " port
 echo -e ""
@@ -70,3 +60,5 @@ menu
 echo "Please enter an correct number"
 ;;
 esac
+menu
+fi
