331e56ed28a71308d01f0ff9979e9b4a0e60782f
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 03:25:56 +0700

    Update addtrgo.sh

--- addtrgo.sh
+++ addtrgo.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
 uuid=$(cat /etc/trojan-go/uuid.txt)
 source /var/lib/gandring/ipvps.conf
