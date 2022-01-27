475bbd54baea456b2befa8164f57285ac7d07f0a
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 12:16:28 +0700

    Update delv2ray.sh

--- delv2ray.sh
+++ delv2ray.sh
@@ -13,7 +13,7 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ifconfig.me/ip);
-clear
+echo -e "checking vps"
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/v2ray-tls.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
@@ -50,6 +50,6 @@ echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Script By gandring"
+echo "Repacked By gandring"
 menu
 fi
