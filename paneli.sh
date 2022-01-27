b90945d378bf28eb3b8f8ecbe23da8b40b054fef
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 08:48:11 +0700

    Update paneli.sh

--- paneli.sh
+++ paneli.sh
@@ -8,16 +8,7 @@ color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
 MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
+echo "Checking vps"
 cat /usr/bin/bannerVPN | lolcat
 echo -e""
 echo -e "${color1}1${color3}.$bd  Secure Socket Tunneling Protocol (${color2}ms-sstp${color3})"
@@ -51,3 +42,5 @@ sleep 1
 paneli
 ;;
 esac
+menu
+fi
