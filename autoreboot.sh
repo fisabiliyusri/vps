dd56f11b1caa13cd9871f9840cf3c1fbd7443ad6
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 11:49:43 +0700

    Update autoreboot.sh

--- autoreboot.sh
+++ autoreboot.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
-clear
+echo -e "checking vps"
 echo -e ""
 echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤" | lolcat
 echo -e "             AUTO REBOOT"
