6363de475dc96b47f48543e717d87f296fe46cd5
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 07:13:14 +0700

    Update and rename ppt.sh to pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -7,16 +7,8 @@ bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-MYIP=$(wget -qO- ifconfig.co);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 echo -e ""
 cat /usr/bin/bannerPPTP | lolcat
@@ -47,8 +39,10 @@ x)
 paneli
 ;;
 *)
-echo "Masukkan Nomor Yang Ada Sayang!"
+echo "Masukkan Nomor Yang Ada"
 sleep 1
 ppt
 ;;
 esac
+menu
+fi
