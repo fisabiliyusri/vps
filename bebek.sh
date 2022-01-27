8c884038980df0bb3b0c5a9de0818ecf6504b6b7
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 11:51:51 +0700

    Update bebek.sh

--- bebek.sh
+++ bebek.sh
@@ -7,17 +7,8 @@ bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-MYIP=$(wget -qO- ifconfig.co);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
 cat /usr/bin/bannerBACKUP | lolcat
 echo -e""
 echo -e "${color1}1${color3}.$bd Autobackup Data VPS (${color2}autobackup${color3})"
@@ -41,7 +32,7 @@ x)
 menu
 ;;
 *)
-echo "Masukkan Nomor Yang Ada Sayang!"
+echo "Masukkan Nomor Yang Ada"
 sleep 1
 bebek
 ;;
