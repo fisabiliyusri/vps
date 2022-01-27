cb041266965a60f6cdeb053d8841504993023b5d
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 12:39:30 +0700

    Update porttrojan.sh

--- porttrojan.sh
+++ porttrojan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/geovpn
+# My Telegram : https://t.me/pegasusq_governor
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,24 +12,14 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
-exit 0
-fi
-clear
 tr="$(cat ~/log-install.txt | grep -w "Trojan" | cut -d: -f2|sed 's/ //g')"
-echo -e "======================================"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e ""
 echo -e "Change Port $tr"
 echo -e ""
-echo -e "======================================"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤"
 read -p "New Port Trojan : " tr2
 if [ -z $tr2 ]; then
 echo "Please Input Port"
