ed8c6a514d162fe32a86775c625b56b7ce97885c
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 12:29:54 +0700

    Update member.sh

--- member.sh
+++ member.sh
@@ -2,9 +2,8 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-clear
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "USERNAME          EXP DATE          STATUS"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
