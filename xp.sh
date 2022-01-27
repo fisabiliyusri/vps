213cc7ffa9d6d8a61d6ca523c4a9cbcd8eac4af6
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 15:43:22 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/gandring
+# My Telegram : https://t.me/pegasusq_governor
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,33 +12,33 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-clear
-data=( `cat /var/lib/geovpnstore/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo -e "checking vps"
+data=( `cat /var/lib/gandring/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### $user" "/var/lib/geovpnstore/data-user-l2tp" | cut -d ' ' -f 3)
+exp=$(grep -w "^### $user" "/var/lib/gandring/data-user-l2tp" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/geovpnstore/data-user-l2tp"
+sed -i "/^### $user $exp/d" "/var/lib/gandring/data-user-l2tp"
 sed -i '/^"'"$user"'" l2tpd/d' /etc/ppp/chap-secrets
 sed -i '/^'"$user"':\$1\$/d' /etc/ipsec.d/passwd
 chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
 fi
 done
-data=( `cat /var/lib/geovpnstore/data-user-pptp | grep '^###' | cut -d ' ' -f 2`);
+data=( `cat /var/lib/gandring/data-user-pptp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### $user" "/var/lib/geovpnstore/data-user-pptp" | cut -d ' ' -f 3)
+exp=$(grep -w "^### $user" "/var/lib/gandring/data-user-pptp" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/geovpnstore/data-user-pptp"
+sed -i "/^### $user $exp/d" "/var/lib/gandring/data-user-pptp"
 sed -i '/^"'"$user"'" pptpd/d' /etc/ppp/chap-secrets
 chmod 600 /etc/ppp/chap-secrets*
 fi
@@ -77,16 +77,16 @@ cd
 fi
 done
 /etc/init.d/ssrmu restart
-data=( `cat /var/lib/geovpnstore/data-user-sstp | grep '^###' | cut -d ' ' -f 2`);
+data=( `cat /var/lib/gandring/data-user-sstp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### $user" "/var/lib/geovpnstore/data-user-sstp" | cut -d ' ' -f 3)
+exp=$(grep -w "^### $user" "/var/lib/gandring/data-user-sstp" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/geovpnstore/data-user-sstp"
+sed -i "/^### $user $exp/d" "/var/lib/gandring/data-user-sstp"
 sed -i '/^'"$user"'/d' /home/sstp/sstp_account
 fi
 done
