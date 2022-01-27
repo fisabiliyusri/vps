0963beeeb226e7fa5abb2314c51ef82583640f70
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-27 11:32:08 +0700

    Update addhost.sh

--- addhost.sh
+++ addhost.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 MYIP=$(wget -qO- ifconfig.me/ip);
-clear
+echo -e "checking vps"
 read -rp "Domain/Host : " -e domain
 echo "IP=$domain" >>/var/lib/gandring/ipvps.conf
 echo $domain > /etc/xray/domain
