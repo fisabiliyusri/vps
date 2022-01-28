5c0397aca003d3fde838ec430061268f77354f8a
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-28 12:26:36 +0700

    Update deltrojan.sh

--- deltrojan.sh
+++ deltrojan.sh
@@ -24,7 +24,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/trojan.json")
 	echo ""
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-	echo " ♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+	echo " ♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 	echo "   nggak ada pengguna kadal"
 	grep -E "^### " "/etc/xray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -42,12 +42,10 @@ systemctl restart xray@trojan
 service cron restart
 clear
 echo ""
-echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "  Trojan-Gfw Akun Dihapus"
-echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo "Trojan-Gfw Akun Dihapus"
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
-echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Repacked By gandring"
-menu
-fi
