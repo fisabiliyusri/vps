c0c672017ccf0eb725ccd316f84241b5031cdea8
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-28 10:53:49 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 clear
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "************************************************"
 echo -e "#      SCRIPT AUTO INSTALL VPN BY gandring      #"
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "*************************************************"
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
 echo -e "# Build Up By gandring                          #"
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♡♤♤♤♤♤♤♤♤♡♤♤♤♤♤♤♤♤♤♤"
+echo -e "*************************************************"
 echo -e "#         PUJI SYUKUR DAN TERIMAKASIH           #"
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♡♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "*************************************************"
 echo -e "# Allah SWT                                     #"
 echo -e "# Muhammad SAW                                  #"
 echo -e "# Keluarga Besarku                              #"
