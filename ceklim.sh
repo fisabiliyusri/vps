c8825a8f6b74f4da148dd1e4d6dc01aa4ff56740
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-28 12:00:08 +0700

    Update ceklim.sh

--- ceklim.sh
+++ ceklim.sh
@@ -20,17 +20,16 @@ echo " ";
 if [ -e "/root/log-limit.txt" ]; then
 echo "User Who Violate The Maximum Limit";
 echo "Time - Username - Number of Multilogin"
-echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤";
+echo "==================================";
 cat /root/log-limit.txt
 else
 echo " No user has committed a violation"
-echo " "
+echo -e ""
 echo " or"
-echo " "
+echo -e ""
 echo " The user-limit script not been executed."
 fi
-echo " ";
-echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤";
 echo ""
-menu
-fi
+echo "=========================================";
+echo ""
+
