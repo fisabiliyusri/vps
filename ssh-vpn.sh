e7734463ca29b531f74d6d74815404354b1b33a0
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-28 10:40:16 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -590,6 +590,8 @@ screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
 history -c
 echo "unset HISTFILE" >> /etc/profile
-
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/ssh-vpn.sh
 #finishing
 clear
