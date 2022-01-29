2088ba680923ad3e65356d503a1a8791b2b4b0f3
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-29 13:53:44 +0700

    Update websocket.py

--- websocket.py
+++ websocket.py
@@ -5,7 +5,8 @@ import socket, threading, thread, select, signal, sys, time, getopt
 LISTENING_ADDR = '0.0.0.0'
 LISTENING_PORT = sys.argv[1]
 else:	
-LISTENING_PORT = 2082
+LISTENING_PORT = '2082'
+LISTENING_PORT 2052
 
 
 # Pass
@@ -15,6 +16,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:200'
+DEFAULT_HOST '127.0.0.222'
 RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
