00521908610604797b0b55348decebab4d3d549e
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-30 02:44:33 +0700

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Simpel Change Root For Linux ( ALL LINUX OS )
+# ================================================
+
+# Clear
+clear
+rm -f root.sh
+
+# Get Passwd
+read -p "Tolong Input Password Baru Buat User Root : " Pass
+
+# Starting Changes To Root
+rm -f /etc/ssh/sshd_config
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/Gandring15/vps/main/sshd
+echo -e "$Pass\n$Pass\n"|passwd root &> /dev/null
+IPANDA=$(wget -qO- ifconfig.co);
+
+# Successful
+clear
+echo "Please Save This VPS Account Information" | lolcat
+echo "============================================" | lolcat
+echo "  IP ADDRESS = $IPANDA" | lolcat
+echo "  Username   = root" | lolcat
+echo "  Password   = $Pass" | lolcat
+echo "============================================" | lolcat
+echo ""
+#echo "VPS Will Rebooting On 5 Second !!!!!"
+
+#rm -f root.sh
+#sleep 5
+#reboot
