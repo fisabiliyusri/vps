f8e4cecd0708fd48a27555c81f5369b2898388c8
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 07:39:03 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,15 +6,15 @@ NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo -e "checking vps"
 
-if [ ! -e /home/vps/public_html/TCP.ovpn ]; then
-cp /etc/openvpn/client-tcp-700.ovpn /home/vps/public_html/tcp.ovpn
-cp /etc/openvpn/client-udp-800.ovpn /home/vps/public_html/udp.ovpn
-cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/ssl.ovpn
+if [ ! -e /home/vps/public_html/config-tcp.ovpn ]; then
+cp /etc/openvpn/client-tcp-3268.ovpn /home/vps/public_html/config-tcp.ovpn
+cp /etc/openvpn/client-udp-3224.ovpn /home/vps/public_html/config-udp.ovpn
+cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/config-ssl.ovpn
 
 mkdir /root/OpenVPN
 cp -r /etc/openvpn/client-tcp-ssl.ovpn OpenVPN/ssl.ovpn
-cp -r /etc/openvpn/client-udp-800.ovpn OpenVPN/udp.ovpn
-cp -r /etc/openvpn/client-tcp-700.ovpn OpenVPN/tcp.ovpn
+cp -r /etc/openvpn/client-udp-3224.ovpn OpenVPN/udp.ovpn
+cp -r /etc/openvpn/client-tcp-3268.ovpn OpenVPN/tcp.ovpn
 cd /root
 zip -r openvpn.zip OpenVPN > /dev/null 2>&1
 cp -r /root/openvpn.zip /home/vps/public_html/openvpn.zip
