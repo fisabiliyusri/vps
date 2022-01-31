d3466f33f5bed387337fadb6590b06a8a811c197
gandring <86754157+Gandring15@users.noreply.github.com>
2022-01-31 09:24:58 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -141,7 +141,7 @@ echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/Gandring15/vps/main/index.html1"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/Gandring15/vps/main/index.html"
 /etc/init.d/nginx restart
 cd
 
