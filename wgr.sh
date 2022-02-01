76883c90dd3722bf6aa962ef184515c8bebb71e1
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:24:46 +0800

    Update wgr.sh

--- wgr.sh
+++ wgr.sh
@@ -7,15 +7,15 @@ bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo -e "checking vps"
 echo -e ""
 cat /usr/bin/bannerWIREGUARD | lolcat
 echo -e""
-echo -e "${color1}1${color3}.$bd Create Wireguard Account (${color2}addwg${color3})"
-echo -e "${color1}2${color3}.$bd Delete Wireguard Account (${color2}delwg${color3})"
-echo -e "${color1}3${color3}.$bd Check User Login Wireguard (${color2}cekwg${color3})"
-echo -e "${color1}4${color3}.$bd Renew Wireguard Account (${color2}renewwg${color3})"
+echo -e "${color1}1${color3}.$bd Buat Akun Wireguard (${color2}addwg${color3})"
+echo -e "${color1}2${color3}.$bd Hapus Akun Wireguard (${color2}delwg${color3})"
+echo -e "${color1}3${color3}.$bd Cek User Login Wireguard (${color2}cekwg${color3})"
+echo -e "${color1}4${color3}.$bd Perpanjang Masa Aktif Akun Wireguard (${color2}renewwg${color3})"
 echo -e""
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e""
