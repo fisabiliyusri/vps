fbb212b88bd9f9ad08cc7b7c25c135d6119b6f5a
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 17:24:51 +0800

    Update delssr.sh

--- delssr.sh
+++ delssr.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo -e "checking vps"
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/shadowsocksr/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -55,4 +55,4 @@ echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Repacked By gandring"
+echo "Not For Sale By @zerossl"
