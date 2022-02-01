039e1468398c469fc194a1cfaafc750b2d8742f6
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 10:38:41 +0800

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/pegasusq_governor
+# My Telegram : https://t.me/Akbar218
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,8 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo -e "checking vps"
+clear
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
@@ -42,7 +41,7 @@ sleep 1
 echo " Please Wait"
 clear
 echo " Autobackup Has Been Started"
-echo " Data Will Be Backed Up Automatically at 00:05 GMT +7"
+echo " Data Will Be Backed Up Automatically at 00:04 GMT +7"
 exit 0
 }
 function stop() {
@@ -56,18 +55,76 @@ clear
 echo " Autobackup Has Been Stopped"
 exit 0
 }
+
+function gantipenerima() {
+rm -rf /home/email
+echo "Please enter your email"
+read -rp "Email : " -e email
+cat <<EOF>>/home/email
+$email
+EOF
+}
+function gantipengirim() {
+echo "Please enter your email"
+read -rp "Email : " -e email
+echo "Please enter your Password email"
+read -rp "Password : " -e pwdd
+rm -rf /etc/msmtprc
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+account default
+host smtp.gmail.com
+port 587
+auth on
+user $email
+from $email
+password $pwdd
+logfile ~/.msmtp.log
+EOF
+}
+function testemail() {
+email=$(cat /home/email)
+if [[ "$email" = "" ]]; then
+start
+fi
+email=$(cat /home/email)
+echo -e "
+Ini adalah isi email percobaaan kirim email dari vps
+IP VPS : $IP
+Tanggal : $date
+" | mail -s "Percobaan Pengiriman Email" $email
+}
 clear
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo -e "♤     Autobackup Data       ♤"
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "=============================="
+echo -e "     Autobackup Data $sts     "
+echo -e "=============================="
 echo -e "1. Start Autobackup"
 echo -e "2. Stop Autobackup"
-echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "3. Ganti Email Penerima"
+echo -e "4. Ganti Email Pengirim"
+echo -e "5. Test kirim Email"
+echo -e "=============================="
 read -rp "Please Enter The Correct Number : " -e num
-if [[ "$num" = "1" ]]; then
+case $num in
+1)
 start
-elif [[ "$num" = "2" ]]; then
+;;
+2)
 stop
-else
+;;
+3)
+gantipenerima
+;;
+4)
+gantipengirim
+;;
+5)
+testemail
+;;
+*)
 clear
-echo " You Entered The Wrong Number"
+;;
+esac
