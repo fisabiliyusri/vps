500c3cbf74e006523634271cabd8b5a71ef34d75
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:17:25 +0800

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo -e "checking vps"
 data=( `cat /var/lib/gandring/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
