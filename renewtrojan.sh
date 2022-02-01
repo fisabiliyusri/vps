33be0edbdbbc01dc7c4a6ff9a6c793e794bd3195
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:50:51 +0800

    Update renewtrojan.sh

--- renewtrojan.sh
+++ renewtrojan.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/trojan.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -53,4 +53,4 @@ echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤
 echo " Username    : $user"
 echo " Kadaluarsa  : $exp4"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Repacked By gandring"
+echo "Not For Sale By @zerossl"
