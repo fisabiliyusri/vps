9379d0cfd1f1360b0471a66734ed0deb3c196108
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:41:17 +0800

    Update renewpptp.sh

--- renewpptp.sh
+++ renewpptp.sh
@@ -12,8 +12,8 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
+IP=$(wget -qO- ipinfo.io/ip);
+
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/gandring/data-user-pptp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
@@ -48,9 +48,9 @@ sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/gandring/data-user-pptp
 clear
 echo ""
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Perpanjang Akun PPTP"
+echo "   Perpanjang Akun PPTP"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username     : $user"
 echo "Kadaluarsa   : $exp4"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Repacked By gandring"
+echo "Not For Sale By @zerossl"
