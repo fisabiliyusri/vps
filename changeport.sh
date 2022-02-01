be078ff0ef65ec3ed3e18ef55876852dd994b72a
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 17:12:21 +0800

    Update changeport.sh

--- changeport.sh
+++ changeport.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo -e "checking vps"
 echo -e ""
 echo -e "     MENU GANTI PORT LAYANAN"
