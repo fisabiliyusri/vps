322353df23227fd6cf656e722cc58049ab0f0136
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 16:11:33 +0800

    Update addsstp.sh

--- addsstp.sh
+++ addsstp.sh
@@ -12,7 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ifconfig.me/ip);
 echo -e "checking vps"
 source /var/lib/gandring/ipvps.conf
 if [[ "$IP2" = "" ]]; then
