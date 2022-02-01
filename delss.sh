846a6fd1822deae8216b86633dfdbe12213c452e
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 17:22:20 +0800

    Update delss.sh

--- delss.sh
+++ delss.sh
@@ -12,8 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
+IP=$(wget -qO- ipinfo.io/ip);
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/shadowsocks-libev/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -59,4 +58,4 @@ echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Script By gandring"
+echo "Not For Sale By @zerossl"
