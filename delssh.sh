cec684c9da000159267fc8e4ad4b2dca7d6a2461
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 17:23:47 +0800

    Update delssh.sh

--- delssh.sh
+++ delssh.sh
@@ -12,8 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
+IP=$(wget -qO- ipinfo.io/ip);
 clear
 read -p "Username SSH to Delete : " Pengguna
 
