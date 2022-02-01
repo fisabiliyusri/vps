212497e5986b3c45f3f43a7447dffb7a758fb2d7
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 20:32:00 +0800

    Update l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -7,23 +7,14 @@ bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
+IP=$(wget -qO- ipinfo.io/ip);
 clear
 echo -e ""
 cat /usr/bin/bannerL2TP | lolcat
 echo -e""
-echo -e "${color1}1${color3}.$bd Creating L2TP Account (${color2}addl2tp${color3})"
-echo -e "${color1}2${color3}.$bd Deleting L2TP Account (${color2}dell2tp${color3})"
-echo -e "${color1}3${color3}.$bd Renew L2TP Account (${color2}renewl2tp${color3})"
+echo -e "${color1}1${color3}.$bd Buat AkunL2TP (${color2}addl2tp${color3})"
+echo -e "${color1}2${color3}.$bd Hapus Akun L2TP (${color2}dell2tp${color3})"
+echo -e "${color1}3${color3}.$bd Perpanjang Masa Aktif Akun L2TP (${color2}renewl2tp${color3})"
 echo -e ""
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e ""
