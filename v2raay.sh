338db91bb1495ce030079884128ac9eaa7834ef9
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:36:20 +0800

    Update v2raay.sh

--- v2raay.sh
+++ v2raay.sh
@@ -7,22 +7,21 @@ bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo -e "checking vps"
+IP=$(wget -qO- ipinfo.io/ip);
 cat /usr/bin/bannerV2RAY | lolcat
 echo -e""
-echo -e "${color1}1${color3}.$bd Create Vmess Websocket Account (${color2}addv2ray${color3})"
-echo -e "${color1}2${color3}.$bd Delete Vmess Websocket Account (${color2}delv2ray${color3})"
+echo -e "${color1}1${color3}.$bd Buat Akun Vmess Websocket (${color2}addv2ray${color3})"
+echo -e "${color1}2${color3}.$bd Hapus AkunVmess Websocket (${color2}delv2ray${color3})"
 echo -e "${color1}3${color3}.$bd Renew Vmess Account (${color2}renewv2ray${color3})"
-echo -e "${color1}4${color3}.$bd Check User Login Vmess (${color2}cekv2ray${color3})"
-echo -e "${color1}5${color3}.$bd Create Vless Websocket Account (${color2}addvless${color3})"
-echo -e "${color1}6${color3}.$bd Deleting Vless Websocket Account (${color2}delvless${color3})"
-echo -e "${color1}7${color3}.$bd Renew Vless Account (${color2}renewvless${color3})"
-echo -e "${color1}8${color3}.$bd Check User Login Vless (${color2}cekvless${color3})"
+echo -e "${color1}4${color3}.$bd Cek User Login Vmess (${color2}cekv2ray${color3})"
+echo -e "${color1}5${color3}.$bd Buat Akun Vless Websocket (${color2}addvless${color3})"
+echo -e "${color1}6${color3}.$bd Hapus Akun Vless Websocket (${color2}delvless${color3})"
+echo -e "${color1}7${color3}.$bd Hapus Akun Vless (${color2}renewvless${color3})"
+echo -e "${color1}8${color3}.$bd Cek User Login Vless (${color2}cekvless${color3})"
 echo -e""
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e""
-read -p " Please Enter The Number  [1-8 or x] :  "  v2ray
+read -p " silahkan masukkan nomor [1-8 or x] :  "  v2ray
 echo -e ""
 case $v2ray in
 1)
