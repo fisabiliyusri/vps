95f6b880124890cfa71a61b3c2bd1e2a0c1ca24f
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 18:42:34 +0800

    Update renewss.sh

--- renewss.sh
+++ renewss.sh
@@ -12,8 +12,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
+IP=$(wget -qO- ipinfo.io/ip);
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/shadowsocks-libev/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
@@ -53,4 +52,4 @@ echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo "Username  : $user"
 echo "Kadaluarsa   : $exp4"
 echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
-echo "Repacked By gandring"
+echo "Not For Sale By @zerossl"
