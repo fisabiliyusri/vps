ba7eeacd7d90b9aeded38a79a2b04b309c8afbed
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-01 20:25:59 +0800

    Update member.sh

--- member.sh
+++ member.sh
@@ -2,7 +2,7 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 echo "=========================================="
 echo "USERNAME          EXP DATE          STATUS"
