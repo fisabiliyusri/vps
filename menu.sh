eeb4f53a2939454fc8311eadc14d9bfbf3ba7e7c
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-06 07:33:26 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -34,43 +34,43 @@ tnggl=$(date +"%d-%B-%Y")
 	tram=$( free -m | awk 'NR==2 {print $2}' )
 	swap=$( free -m | awk 'NR==4 {print $2}' )
 	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
- echo -e  "$bl ║                                 [ INFORMASI VPS ]                               ║" |lolcat
- echo -e  "$op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"| lolcat
- echo -e " $bl ║ \e[36;1m• ISP Name  :\e[0m$bd $ISP"                                             ║"
- echo -e " $bl ║ \e[36;1m• City      :\e[0m$bd $CITY"                                            ║"
- echo -e " $bl ║ \e[36;1m• CPU Model :\e[0m$bd$cname"                                            ║"
- echo -e " $bl ║ \e[36;1m• Cores     :\e[0m$bd $cores"                                           ║"
- echo -e " $gl ║ \e[36;1m• CPU Freq  :\e[0m$bd$freq MHz"                                         ║"
- echo -e " $gl ║ \e[36;1m• Total RAM :\e[0m$bd $tram MB"                                         ║"
- echo -e " $gl ║ \e[36;1m• Waktu     :\e[0m$bd $jam"                                             ║"
- echo -e " $mg ║ \e[36;1m• Hari      :\e[0m$bd $hari"                                            ║"
- echo -e " $mg ║ \e[36;1m• Tanggal   :\e[0m$bd $tnggl"                                           ║"
- echo -e " $mg ║ \e[36;1m• IP VPS    :\e[0m$bd $IPVPS"                                           ║"
- echo -e " $mg ║ \e[36;1m• Domain    :\e[0m$bd $domain"                                          ║"
- echo -e  "  \e[31;1m_____________________________________________________________________________║" | lolcat
- echo -e  "  ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
- echo -e  "  ║                                 [ MENU TUNNELING ]                                ║" |lolcat
- echo -e  "  ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
- echo -e  "\e[31;2m_______________________________________________________________________________║"| lolcat
- echo -e  " $gl ║\e[m$red 1$bl.\e[m$bd Menu Layanan SSH & OpenVPN       $bl                      ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
- echo -e  " $gl ║\e[m$red 2$bl.\e[m$bd Menu Layanan SSTP - L2TP - PPTP - Wireguard      $bl      ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
- echo -e  " $mg ║\e[m$red 3$bl.\e[m$bd Menu Layanan Shadowsocks & ShadowsocksR         $gl       ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
- echo -e  " $mg ║\e[m$red 4$bl.\e[m$bd Menu Layanan V2ray Vmess & Vless            $gl           ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
- echo -e  " $bl ║\e[m$red 5$bl.\e[m$bd Menu Layanan Trojan-Gfw & Trojan-GO       $mg             ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
- echo -e  " $bl ║\e[m$red 6$bl.\e[m$bd Menu Backup         $mg                                   ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
- echo -e  " $mg ║\e[m$red 7$bl.\e[m$bd Menu Konfigurasi System            $mg                    ║"
- echo -e  " $op ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
- echo -e  " $mg ║\e[m$red 8$bl.\e[m$bd Menu Pembaruan Script $gl Version $op($bl 2.0.4 $op) $mg  ║"
- echo -e  " $bl ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
- echo -e  " $rd ║ x.   Exit                                                                      ║" | lolcat
- echo -e  " $bl ║.♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
-echo -e  ""
+ echo -e  "$bl ║              [ INFORMASI VPS ]               ║" |lolcat
+ echo -e  "$op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"| lolcat
+ echo -e " $bl ║ISP Name :$ISP"                               ║"
+ echo -e " $bl ║City     :$CITY"                              ║"
+ echo -e " $bl ║$cname"                                       ║"
+ echo -e " $bl ║Cores    :$cores"                             ║"
+ echo -e " $gl ║CPU Freq :$freq MHz"                          ║"
+ echo -e " $gl ║Total RAM:$tram MB"                           ║"
+ echo -e " $gl ║Waktu    :$jam"                               ║"
+ echo -e " $mg ║Hari     :$hari"                              ║"
+ echo -e " $mg ║Tanggal  :$tnggl"                             ║"
+ echo -e " $mg ║IP VPS   :$IPVPS"                             ║"
+ echo -e " $mg ║Domain   :$domain"                            ║"
+ echo -e  "  \e[31;1m__________________________________________║" | lolcat
+ echo -e  "  ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
+ echo -e  "  ║                [ MENU TUNNELING ]              ║" |lolcat
+ echo -e  "  ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
+ echo -e  "\e[31;2m_____________________________________________________________"| lolcat
+ echo -e  " $gl ║\e[m$red 1$bl.\e[m$bd Menu Layanan SSH & OpenVPN       $bl    ║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
+ echo -e  " $gl ║\e[m$red 2$bl.\e[m$bd Menu Layanan SSTP-L2TP-PPTP-Wireguard$bl║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
+ echo -e  " $mg ║\e[m$red 3$bl.\e[m$bd Menu Layanan ShadowsocksR & OBFS/HTTP$gl║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
+ echo -e  " $mg ║\e[m$red 4$bl.\e[m$bd Menu Layanan V2ray Vmess & Vless$gl     ║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
+ echo -e  " $bl ║\e[m$red 5$bl.\e[m$bd Menu Layanan Trojan-Gfw & Trojan-GO$mg  ║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║"
+ echo -e  " $bl ║\e[m$red 6$bl.\e[m$bd Menu Backup         $mg                 ║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
+ echo -e  " $mg ║\e[m$red 7$bl.\e[m$bd Menu Konfigurasi System            $mg  ║"
+ echo -e  " $op ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║
+ echo -e  " $mg ║\e[m$red 8$bl.\e[m$bd Menu Pembaruan Script        V 2.3.4$mg ║"
+ echo -e  " $bl ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
+ echo -e  " $rd ║ x.   Exit                                                    ║" | lolcat
+ echo -e  " $bl ║♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤║" | lolcat
+ echo -e  ""
 read -p "       silahkan Pilih Angka[1-8 or x] :  " menu
 echo -e   ""
 echo -e   ""
