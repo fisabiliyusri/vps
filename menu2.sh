954a42233f66249a02d1b16dcd7937f9bc7b42d2
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-06 05:43:26 +0800

    Update menu2.sh

--- menu2.sh
+++ menu2.sh
@@ -6,12 +6,13 @@ PURPLE="\033[0;1;32m"
 BLUE="\033[0;1;33m"
 PINK="\033[0m"
 
-echo -e "==================================================="
-echo -e "             PREMIUM SCRIPT INSTALL VPS"
-echo -e "                MENU LAYANAN SERVER"             
-echo -e "=================================================="
-echo -e "        LAYANAN SSH & OpenVPN"
-echo -e "========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "      PREMIUM SCRIPT INSTALL VPS"
+echo -e "         MENU LAYANAN SERVER"             
+echo -e "            Luxury Edition"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "         LAYANAN SSH & OpenVPN"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 1$y.Buat Akun SSH & OpenVPN"
 echo -e "$yy 2$y.Buat Trial Akun SSH & OpenVPN"
 echo -e "$yy 3$y.Perpanjang Akun SSH & OpenVPN"
@@ -22,93 +23,100 @@ echo -e "$yy 7$y.Hapus User SSH & OpenVPN Kadaluarsa"
 echo -e "$yy 8$y.Set up Autokill SSH"
 echo -e "$yy 9$y.Tampilkan User Multi Login SSH"
 echo -e "$yy 10$y.Restart Layanan"
-echo -e "=========================================="
-echo -e "              LAYANAN L2TP"
-echo -e "=========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "               LAYANAN L2TP"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 11$y.Buat Akun L2TP"
 echo -e "$yy 12$y.Hapus Akun L2TP"
 echo -e "$yy 13$y.Perpanjang Masa Aktif Akun L2TP"
-echo -e "================================/=/========"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "              LAYANAN PPTP"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 14$y.Buat Akun PPTP"
 echo -e "$yy 15$y.Hapus Akun PPTP"
 echo -e "$yy 16$y.Perpanjang Masa Aktif Akun PPTP"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "              LAYANAN SSTP"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 17$y.Buat Akun SSTP"
 echo -e "$yy 18$y.Hapus Akun SSTP"
 echo -e "$yy 19$y.Perpanjang Masa Aktif  Akun SSTP"
 echo -e "$yy 20$y.Cek User Login SSTP"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "            LAYANAN WIREGUARD"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 21$y.Buat Akun Wireguard"
 echo -e "$yy 22$y.Hapus Akun Wireguard"
 echo -e "$yy 23$y.Perpanjang Masa Aktif Akun Wireguard"
-echo -e "============================================"
-echo -e "       LAYANAN SHADOWSOCKS OBFS/HTTP"
-echo -e "============================================"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "      LAYANAN SHADOWSOCKS OBFS/HTTP"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 24$y.Buat Akun Shadowsocks"
 echo -e "$yy 25$y.Hapus Akun Shadowsocks"
 echo -e "$yy 26$y.Perpanjang Masa Aktif Akun Shadowsocks"
 echo -e "$yy 27$y.Cek User Login Shadowsocks"
-echo -e "==============================================="
-echo -e "           LAYANANAN SHADOWSOCKSR"
-echo -e "==============================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "          LAYANANAN SHADOWSOCKSR"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 28$y.Buat Akun ShadowsocksR"
 echo -e "$yy 29$y.Hapus Akun ShadowsocksR"
 echo -e "$yy 30$y.Perpanjang Masa Aktif ShadowsocksR"
 echo -e "$yy 31$y.Lihat Menu Lain ShadowsocksR"
-echo -e "==============================================="
-echo -e "                LAYANAN VMESS"
-echo -e "==============================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "             LAYANAN VMESS"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 32$y.Buat Akun Vmess Websocket"
 echo -e "$yy 33$y.Hapus Akun Vmess Websocket"
 echo -e "$yy 34$y.Perpanjang Masa Aktif Akun Vmess"
 echo -e "$yy 35$y.Cek User Login Vmess"
 echo -e "$yy 36$y.Pembarui Certificate XRAYS Akun"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "              LAYANAN VLESS"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "$yy 37$y.Buat Akun Vless Websocket"
 echo -e "$yy 38$y.Hapus Akun Vless Websocket"
 echo -e "$yy 39$y.Perpanjang Masa Aktif Vless "
 echo -e "$yy 40$y.Cek User Login Vless"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "          LAYANAN VLESS GRPC"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "$yy 41$y.Buat Akun Vless gRPC"
+echo -e "$yy 42$y.Hapus Akun Vless gRPC"
+echo -e "$yy 43$y.Perpanjang Akun Vless gRPC"
+echo -e "$yy 44$y.Cek User Login Vless gRPC"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "          LAYANAN TROJAN-GFW"
-echo -e "==========================================="
-echo -e "$yy 41$y.Buat Akun Trojan-Gfw"
-echo -e "$yy 42$y.Hapus Akun Trojan-Gfw"
-echo -e "$yy 43$y.Perpanjang Masa Aktif Trojan-Gfw"
-echo -e "$yy 44$y.Cek User Login Trojan-Gfw"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "$yy 45$y.Buat Akun Trojan-Gfw"
+echo -e "$yy 46$y.Hapus Akun Trojan-Gfw"
+echo -e "$yy 47$y.Perpanjang Masa Aktif Trojan-Gfw"
+echo -e "$yy 48$y.Cek User Login Trojan-Gfw"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "           LAYANAN TROJAN-GO"
-echo -e "==========================================="
-echo -e "$yy 45$y.Buat Akun Trojan-Go"
-echo -e "$yy 46$y.Hapus AkunTrojan-Go"
-echo -e "$yy 47$y.Perpanjang Masa Aktif Trojan-Go"
-echo -e "$yy 48$y.Cek User Login Trojan-Go"
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "$yy 49$y.Buat Akun Trojan-Go"
+echo -e "$yy 50$y.Hapus AkunTrojan-Go"
+echo -e "$yy 51$y.Perpanjang Masa Aktif Trojan-Go"
+echo -e "$yy 52$y.Cek User Login Trojan-Go"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "                  SYSTEM"
-echo -e "==========================================="
-echo -e "$yy 49$y.Add Atau Ganti Subdomain Host Di VPS"
-echo -e "$yy 50$y.Ganti Port Beberapa Layanan"
-echo -e "$yy 51$y.Autobackup Data VPS"
-echo -e "$yy 52$y.Backup Data VPS"
-echo -e "$yy 53$y.Restore Data VPS"
-echo -e "$yy 54$y.Webmin Menu"
-echo -e "$yy 55$y.Set Untuk Limitasi Sepit Server"
-echo -e "$yy 56$y.Cek Penggunaan Ram Cpu VPS"
-echo -e "$yy 57$y.Reboot VPS"
-echo -e "$yy 58$y.Speedtest VPS"
-echo -e "$yy 59$y.Tampilkan Informasi System Vps"
-echo -e "$yy 60$y.Info Script Auto Install"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "$yy 53$y.Add Atau Ganti Subdomain Host Di VPS"
+echo -e "$yy 54$y.Ganti Port Beberapa Layanan"
+echo -e "$yy 55$y.Autobackup Data VPS"
+echo -e "$yy 56$y.Backup Data VPS"
+echo -e "$yy 57$y.Restore Data VPS"
+echo -e "$yy 58$y.Webmin Menu"
+echo -e "$yy 59$y.Set Untuk Limitasi Sepit Server"
+echo -e "$yy 60$y.Cek Penggunaan Ram Cpu VPS"
+echo -e "$yy 61$y.Reboot VPS"
+echo -e "$yy 62$y.Speedtest VPS"
+echo -e "$yy 63$y.Tampilkan Informasi System Vps"
+echo -e "$yy 64$y.Info Script Auto Install"
 echo -e ""
-echo -e "==========================================="
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e ""
-read -p "Select From Options [ 1 - 60 ] : " menu
+read -p "Silahkan Pilih Angka [ 1 - 64 ] : " menu
 echo -e ""
 case $menu in
 1)
@@ -232,63 +240,75 @@ renewvless
 cekvless
 ;;
 41)
-addtrojan
+addvless-grpc
 ;;
 42)
-deltrojan
+delvless-grpc
 ;;
 43)
-renewtrojan
+renewvless-grpc
 ;;
 44)
-cektrojan
+cekvless-grpc
 ;;
 45)
-addtrgo
+addtrojan
 ;;
 46)
-deltrgo
+deltrojan
 ;;
 47)
-renewtrgo
+renewtrojan
 ;;
 48)
-cektrgo
+cektrojan
 ;;
 49)
-addhost
+addtrgo
 ;;
 50)
-changeport
+deltrgo
 ;;
 51)
-autobackup
+renewtrgo
 ;;
 52)
-backup
+cektrgo
 ;;
 53)
-restore
+addhost
 ;;
 54)
-webmin
+changeport
 ;;
 55)
-limitspeed
+autobackup
 ;;
 56)
-ram
+backup
 ;;
 57)
-reboot
+restore
 ;;
 58)
-speedtest
+webmin
 ;;
 59)
-info
+limitspeed
 ;;
 60)
+ram
+;;
+61)
+reboot
+;;
+62)
+speedtest
+;;
+63)
+info
+;;
+64)
 about
 ;;
 *)
