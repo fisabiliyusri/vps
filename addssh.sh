fa1557f4da96b8cea1cc1aa43a7ac3fe9be9a484
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-06 01:47:48 +0800

    Update addssh.sh

--- addssh.sh
+++ addssh.sh
@@ -35,7 +35,8 @@ echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e ""
-echo -e "♤♤♤Informasi SSH & OpenVPN♤♤♤"
+echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
+echo -e "♤  Informasi SSH & OpenVPN   ♤"
 echo -e "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤"
 echo -e "IP/Host       : $IP"
 echo -e "Domain        : ${domain}"
