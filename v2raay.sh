dd0d72332c76ba29636d5a5d3995ae864658511c
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-06 05:56:16 +0800

    Update v2raay.sh

--- v2raay.sh
+++ v2raay.sh
@@ -12,12 +12,16 @@ cat /usr/bin/bannerV2RAY | lolcat
 echo -e""
 echo -e "${color1}1${color3}.$bd Buat Akun Vmess Websocket (${color2}addv2ray${color3})"
 echo -e "${color1}2${color3}.$bd Hapus AkunVmess Websocket (${color2}delv2ray${color3})"
-echo -e "${color1}3${color3}.$bd Renew Vmess Account (${color2}renewv2ray${color3})"
+echo -e "${color1}3${color3}.$bd Perpanjang Akun Vmess websocket(${color2}renewv2ray${color3})"
 echo -e "${color1}4${color3}.$bd Cek User Login Vmess (${color2}cekv2ray${color3})"
 echo -e "${color1}5${color3}.$bd Buat Akun Vless Websocket (${color2}addvless${color3})"
 echo -e "${color1}6${color3}.$bd Hapus Akun Vless Websocket (${color2}delvless${color3})"
-echo -e "${color1}7${color3}.$bd Hapus Akun Vless (${color2}renewvless${color3})"
+echo -e "${color1}7${color3}.$bd Perpanjang Akun Vless Websocket(${color2}renewvless${color3})"
 echo -e "${color1}8${color3}.$bd Cek User Login Vless (${color2}cekvless${color3})"
+echo -e "${color1}9${color3}.$bd Buat Akun Vless gRPC (${color2}cekvless-grpc${color3})"
+echo -e "${color1}10${color3}.$bd Hapus Akun Vless gRPC (${color2}cekvless-grpc${color3})"
+echo -e "${color1}11${color3}.$bd Perpanjang Akun Vless gRPC (${color2}cekvless-grpc${color3})"
+echo -e "${color1}12${color3}.$bd Cek User Login Vless gRPC (${color2}cekvless-grpc${color3})"
 echo -e""
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e""
@@ -47,6 +51,18 @@ renewvless
 ;;
 8)
 cekvless
+;;
+9)
+addvless-grpc
+;;
+10)
+delvless-grpc
+;;
+11)
+renewvless-grpc
+;;
+12)
+cekvless-grpc
 ;;
 x)
 menu
