a34f036e030714ff09c09e092c5ec19fe03158d4
gandring <86754157+Gandring15@users.noreply.github.com>
2022-02-06 06:33:06 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -36,17 +36,17 @@ tnggl=$(date +"%d-%B-%Y")
 	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
  echo -e  "  ║                                  [ INFORMASI VPS ]                               ║" |lolcat
  echo -e  "  ╠♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤╣"| lolcat
- echo -e " $bl ║ \e[36;1m• ISP Name  :\e[0m$bd $ISP"                                            ║
- echo -e " $bl ║ \e[36;1m• City      :\e[0m$bd $CITY"                                           ║
- echo -e " $bl ║ \e[36;1m• CPU Model :\e[0m$bd$cname"                                           ║
- echo -e " $bl ║ \e[36;1m• Cores     :\e[0m$bd $cores"                                          ║
- echo -e " $gl ║ \e[36;1m• CPU Freq  :\e[0m$bd$freq MHz"                                        ║
- echo -e " $gl ║ \e[36;1m• Total RAM :\e[0m$bd $tram MB"                                        ║
- echo -e " $gl ║ \e[36;1m• Waktu     :\e[0m$bd $jam"                                            ║
- echo -e " $mg ║ \e[36;1m• Hari      :\e[0m$bd $hari"                                           ║
- echo -e " $mg ║ \e[36;1m• Tanggal   :\e[0m$bd $tnggl"                                          ║
- echo -e " $mg ║ \e[36;1m• IP VPS    :\e[0m$bd $IPVPS"                                          ║
- echo -e " $mg ║ \e[36;1m• Domain    :\e[0m$bd $domain"                                         ║
+ echo -e " $bl ║ \e[36;1m• ISP Name  :\e[0m$bd $ISP"                                            ║"
+ echo -e " $bl ║ \e[36;1m• City      :\e[0m$bd $CITY"                                           ║"
+ echo -e " $bl ║ \e[36;1m• CPU Model :\e[0m$bd$cname"                                           ║"
+ echo -e " $bl ║ \e[36;1m• Cores     :\e[0m$bd $cores"                                          ║"
+ echo -e " $gl ║ \e[36;1m• CPU Freq  :\e[0m$bd$freq MHz"                                        ║"
+ echo -e " $gl ║ \e[36;1m• Total RAM :\e[0m$bd $tram MB"                                        ║"
+ echo -e " $gl ║ \e[36;1m• Waktu     :\e[0m$bd $jam"                                            ║"
+ echo -e " $mg ║ \e[36;1m• Hari      :\e[0m$bd $hari"                                           ║"
+ echo -e " $mg ║ \e[36;1m• Tanggal   :\e[0m$bd $tnggl"                                          ║"
+ echo -e " $mg ║ \e[36;1m• IP VPS    :\e[0m$bd $IPVPS"                                          ║"
+ echo -e " $mg ║ \e[36;1m• Domain    :\e[0m$bd $domain"                                         ║"
  echo -e  "  \e[31;1m_____________________________________________________________________________║" | lolcat
  echo -e  "  ╠♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤╣" | lolcat
  echo -e  "  ║                     [ MENU TUNNELING ]                     ║" |lolcat
